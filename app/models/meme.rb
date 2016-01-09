a31f88ebbdf4865484e95dd32305f213e9fd7b87
beliar91 <beliar91@vp.pl>
2016-01-09 17:20:14 +0100

    Adding validation to memes

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -4,6 +4,8 @@ class Meme < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
+  validates :meme, presence: true
+
   after_validation :set_meme_as_inactive, on: :create
 
   def set_meme_as_inactive
